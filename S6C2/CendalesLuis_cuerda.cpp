f8fa82fd5d15862c9e06537a65a736cb666ac1ab
LuisCendales <lc.cendales10@uniandes.edu.co>
2019-07-09 17:05:10 +0000

    adelanto

--- S6C2/CendalesLuis_cuerda.cpp
+++ S6C2/CendalesLuis_cuerda.cpp
@@ -11,7 +11,7 @@ int main(){
     double x0 = 0;
     double dx = 0.05;    
     int n;
-    n=(L-x0)/dx;
+    n=(L-x0)+1/dx;
     
     double xlista[n];
     for(int i=0;i<n;i++){
@@ -20,10 +20,13 @@ int main(){
     }
     
     double tfin = 0.1;
+    double t0 = 0.;
+    double dt= 0.01;
     double A0=0.01;
     double pendiente=A0/(L/2);
     
     double phi[n];
+    double phi_new[n];
     ofstream outfile;
     
     for(int i=0;i<n;i++){
@@ -33,6 +36,22 @@ int main(){
             phi[i]=xlista[i]*(-pendiente)+2*A0;
         }
     }
+    double phi1[n];
+    double r = ((c*c)*(dt*dt))/(dx*dx);
+    for(int i=0;i<n;i++){
+        if(i=0){
+            phi1[i]=(r/2)*(phi[i+1]+phi[i]-2*phi[i]);
+        }else if(i=(n-1)){
+            phi1[i]=(r/2)*(phi[i]+phi[i-1]-2*phi[i]);
+        }else{
+            phi1[i]=(r/2)*(phi[i+1]+phi[i-1]-2*phi[i]);
+        }
+    }    
+    
+    
+    
+    
+    
     outfile.open("datos.dat");
     for(int i=0;i<n;i++){
         outfile<<xlista[i]<<" "<<phi[i]<<endl;
