1b6403a10fc2c7afbe272490fdfc0d2d13051642
LuisCendales <lc.cendales10@uniandes.edu.co>
2019-07-22 16:36:04 +0000

    HW1

--- CendalesLuis_hw1/Temperaturas.sh
+++ CendalesLuis_hw1/Temperaturas.sh
@@ -4,6 +4,6 @@ awk 'NR>8{if($13>0 && !($1=="Year"))print $1}' GLB.Ts.txt > TempDic.txt
 
 awk 'NR>8{if($1!="Year" && NF==20)print $1,$14/100;}' GLB.Ts.txt > TempPomedios.txt
 
-python Temperaturasplot.py
+python plotTemp.py
 
 rm GLB.Ts.txt
